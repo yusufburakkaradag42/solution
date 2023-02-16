7805e6d65da1c7d323f9b4f38f912529f7e61dbd
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2023-02-16 21:30:05 +0300

    link

---
+++ src/companents/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.scss'
+const  Header = () => {
+  return (
+    <div className='header'>
+        <span className='text-shadow'> Whoe are You </span>
+    </div>
+  )
+}
+
+export default  Header
